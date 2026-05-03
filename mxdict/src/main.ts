d0bbbfeb65df7ddee3db159a0baf511cddee7354
MouJieQin <724174637@qq.com>
2026-05-03 16:00:06 +0800

    merge spa into tauri

--- mxdict/src/main.ts
+++ mxdict/src/main.ts
@@ -1,4 +1,17 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import '@/style.css'
 
-createApp(App).mount("#app");
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/dist/index.css'
+
+import App from '@/App.vue'
+import router from '@/router'
+import ElementPlus from 'element-plus'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
+
+
+const app = createApp(App)
+const pinia = createPinia()
+app.use(router).use(ElementPlus).use(pinia).mount('#app')
+router.push('/dict/1')
